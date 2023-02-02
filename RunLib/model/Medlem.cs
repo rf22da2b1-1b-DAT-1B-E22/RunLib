e8f6a4090505e2a60a8341485520bc0fc8ac026c
Peter Levinsky <PELE@zealand.dk>
2023-02-02 18:50:10 +0100

    Add project files.

---
+++ RunLib/model/Medlem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunLib.model
+{
+    public class Medlem
+    {
+        private String _navn;
+        private readonly int _medlemsId;
+        private String _mobil;
+        private String _løbeHold;
+        private double _pris;
+
+        public string Navn { 
+            get => _navn;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException("Navn er krævet og skal være mindst 3 tegn langt");
+                }
+                _navn = value;
+            }
+            }
+        public int MedlemsId { get => _medlemsId;  }
+        public string Mobil { 
+            get => _mobil;
+            set {
+                if (string.IsNullOrWhiteSpace(value) )
+                {
+                    throw new ArgumentNullException("Mobil er krævet");
+                }
+                if (value.Length < 8 || 12 < value.Length)
+                {
+                    throw new ArgumentException("mobil skal være mellem 8 - 12 tegn langt");
+                }
+
+                _mobil = value; 
+            }
+        }
+        public string LøbeHold { 
+            get => _løbeHold;
+            set {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("LøbeHold er krævet");
+                }
+                if (!(value == "sort" || value == "blå" || value == "grøn" || value == "gul" || value == "orange" || value == "rød" ))
+                {
+                    throw new ArgumentException("LøbeHold skal være 'sort' 'blå' 'grøn' 'gul' 'orange' eller 'rød'");
+                }
+                _løbeHold = value; 
+            }
+        }
+        public double Pris { 
+            get => _pris;
+            set { 
+                if (value < 50)
+                {
+                    throw new ArgumentException("Pris er mindst 50");
+                }
+                _pris = value; 
+            }
+        }
+
+        public Medlem(string navn, int medlemsId, string mobil, string løbeHold, double pris)
+        {
+            Navn = navn;
+            _medlemsId = medlemsId;
+            Mobil = mobil;
+            LøbeHold = løbeHold;
+            Pris = pris;
+        }
+
+        public Medlem():this("dummy",0,"11111111", "rød", 50)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Navn)}={Navn}, {nameof(MedlemsId)}={MedlemsId.ToString()}, {nameof(Mobil)}={Mobil}, {nameof(LøbeHold)}={LøbeHold}, {nameof(Pris)}={Pris.ToString()}}}";
+        }
+    }
+}
---
+++ RunningApp/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RunLib.model;
+
+Console.WriteLine("Hello, World!");
+
+Medlem medlem1 = new Medlem();
+Medlem medlem2 = new Medlem("Peter",222,"33445566","gul", 120 );
+Console.WriteLine(medlem1);
+Console.WriteLine(medlem2);
+
+try
+{
+    medlem1.Navn = "Pi";
+}
+catch (ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
+
+try
+{
+    medlem1.Mobil = "1234567";
+}
+catch (ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
+
+try
+{
+    medlem1.LøbeHold = "agurk";
+}
+catch (ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
+
+try
+{
+    medlem1.Pris = 49;
+}
+catch (ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
